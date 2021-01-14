fc55da2529e8162421e9fa459d7903340989e7a2
John Connor <zkmd2012@inbox.ru>
2021-01-14 17:42:09 +0500

    logging in added

--- backend/routes/auth-routes.js
+++ backend/routes/auth-routes.js
@@ -3,8 +3,12 @@ const bcrypt = require("bcryptjs"); //for hashing passwords
 const router = express.Router();
 const User = require("../models/user-model");
 const HttpError = require("../models/http-error");
-const { registerValidation } = require("../validation/validation");
+const {
+  registerValidation,
+  loginValidation,
+} = require("../validation/validation");
 
+//POST - user registration
 router.post("/register", async (req, res, next) => {
   //VALIDATE DATA BEFORE
   const validation = registerValidation(req.body);
@@ -33,6 +37,25 @@ router.post("/register", async (req, res, next) => {
   } catch (error) {
     res.status(400).send(error);
   }
+});
+
+//POST - user login
+router.post("/login", async (req, res, next) => {
+  //VALIDATE DATA BEFORE
+  const validation = loginValidation(req.body);
+  if (validation.error) {
+    const error = new HttpError(validation.error.details[0].message, 400);
+    return next(error);
+  }
+
+  //CHECK IF THE USER ALREADY EXISTS
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Email is not found");
+  //CHECK IF THE PASSWORD IS CORRECT
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+  if (!validPass) return res.status(400).send("Invalid password");
+
+  res.send("Logged in");
 });
 
 module.exports = router;
